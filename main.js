ac1235dd960b896841a45cfb11542b3d30e2db08
Aquazeo <zhijiechen@stu.scu.edu.cn>
2025-03-31 18:11:53 +0800

    Notification

---
+++ main.js
@@ -0,0 +1,40 @@
+// Create OS desktop notifications
+//
+// For more info, see:
+// https://electronjs.org/docs/api/notification
+// https://electronjs.org/docs/tutorial/notifications
+
+const { app, BrowserWindow, Notification } = require('electron/main')
+const path = require('node:path')
+
+app.whenReady().then(() => {
+  const mainWindow = new BrowserWindow({
+    height: 600,
+    width: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  if (Notification.isSupported()) {
+    const notification = new Notification({
+      title: 'Hello World!',
+      subtitle: 'Nice to see you',
+      body: 'Are you having a good day?',
+      hasReply: true
+    })
+
+    notification.on('show', () => console.log('Notification shown'))
+    notification.on('click', () => console.log('Notification clicked'))
+    notification.on('close', () => console.log('Notification closed'))
+    notification.on('reply', (event, reply) => {
+      console.log(`Reply: ${reply}`)
+    })
+
+    notification.show()
+  } else {
+    console.log('Hm, are notifications supported on this system?')
+  }
+
+  mainWindow.loadFile('index.html')
+})
